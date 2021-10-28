5efe1f71b6b53b4de9981c7bb89884c71e55343f
Duncan <bonsairobo@gmail.com>
2021-10-28 01:05:00 -0700

    Add documentation about negative strides and modular arithmetic

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Of course, to achieve a different layout, one only needs to choose a different p
 column-major layout would require coordinates specified as `[..., Z, Y, X]`. For a 3D layout where each Y level set is
 contiguous in memory, either layout `[X, Z, Y]` or `[Z, X, Y]` would work.
 
-## Code Example
+## Example: Indexing Multidimensional Arrays
 
 ```rust
 use ndshape::{Shape, ConstShape3u32, ConstShape4u32, ConstPow2Shape3u32, Shape3u32};
@@ -39,7 +39,8 @@ assert_eq!(shape.delinearize(index), [1, 2, 3]);
 
 // Use a shape for indexing an array in 4D.
 // Step X, then Y, then Z, since that results in monotonic increasing indices.
-// (Believe it or not, Rust's N-dimensional array indexing is significantly slower than this).
+// (Believe it or not, Rust's N-dimensional array (e.g. `[[T; N]; M]`)
+// indexing is significantly slower than this).
 let shape = ConstShape4u32::<5, 6, 7, 8>;
 let data = [0; 5 * 6 * 7 * 8];
 for w in 0..8 {
@@ -52,6 +53,33 @@ for w in 0..8 {
         }
     }
 }
+```
+
+## Example: Negative Strides with Modular Arithmetic
+
+It is often beneficial to linearize a negative vector that results in a negative linear "stride." But when using unsigned
+linear indices, a negative stride would require a modular arithmetic representation, where e.g. `-1` maps to `u32::MAX`.
+This works fine with any [`Shape`](crate::Shape). You just need to be sure to use modular arithmetic with the resulting
+linear strides, e.g. [`u32::wrapping_add`](u32::wrapping_add) and [`u32::wrapping_mul`](u32::wrapping_mul). Also, it is not
+possible to delinearize a negative stride with modular arithmetic. For that, you must use signed integer coordinates.
+
+```rust
+use ndshape::{Shape, ConstShape3u32, ConstShape3i32};
+
+let shape = ConstShape3u32::<10, 10, 10>;
+let stride = shape.linearize([0, -1i32 as u32, 0]);
+assert_eq!(stride, -10i32 as u32);
+
+// Delinearize does not work with unsigned coordinates!
+assert_ne!(shape.delinearize(stride), [0, -1i32 as u32, 0]);
+assert_eq!(shape.delinearize(stride), [6, 8, 42949672]);
+
+let shape = ConstShape3i32::<10, 10, 10>;
+let stride = shape.linearize([0, -1, 0]);
+assert_eq!(stride, -10);
+
+// Delinearize works with signed coordinates.
+assert_eq!(shape.delinearize(stride), [0, -1, 0]);
 ```
 
 License: MIT OR Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@
 //! column-major layout would require coordinates specified as `[..., Z, Y, X]`. For a 3D layout where each Y level set is
 //! contiguous in memory, either layout `[X, Z, Y]` or `[Z, X, Y]` would work.
 //!
-//! # Code Example
+//! # Example: Indexing Multidimensional Arrays
 //!
 //! ```
 //! use ndshape::{Shape, ConstShape3u32, ConstShape4u32, ConstPow2Shape3u32, Shape3u32};
@@ -37,7 +37,8 @@
 //!
 //! // Use a shape for indexing an array in 4D.
 //! // Step X, then Y, then Z, since that results in monotonic increasing indices.
-//! // (Believe it or not, Rust's N-dimensional array indexing is significantly slower than this).
+//! // (Believe it or not, Rust's N-dimensional array (e.g. `[[T; N]; M]`)
+//! // indexing is significantly slower than this).
 //! let shape = ConstShape4u32::<5, 6, 7, 8>;
 //! let data = [0; 5 * 6 * 7 * 8];
 //! for w in 0..8 {
@@ -50,6 +51,33 @@
 //!         }
 //!     }
 //! }
+//! ```
+//!
+//! # Example: Negative Strides with Modular Arithmetic
+//!
+//! It is often beneficial to linearize a negative vector that results in a negative linear "stride." But when using unsigned
+//! linear indices, a negative stride would require a modular arithmetic representation, where e.g. `-1` maps to `u32::MAX`.
+//! This works fine with any [`Shape`](crate::Shape). You just need to be sure to use modular arithmetic with the resulting
+//! linear strides, e.g. [`u32::wrapping_add`](u32::wrapping_add) and [`u32::wrapping_mul`](u32::wrapping_mul). Also, it is not
+//! possible to delinearize a negative stride with modular arithmetic. For that, you must use signed integer coordinates.
+//!
+//! ```
+//! use ndshape::{Shape, ConstShape3u32, ConstShape3i32};
+//!
+//! let shape = ConstShape3u32::<10, 10, 10>;
+//! let stride = shape.linearize([0, -1i32 as u32, 0]);
+//! assert_eq!(stride, -10i32 as u32);
+//!
+//! // Delinearize does not work with unsigned coordinates!
+//! assert_ne!(shape.delinearize(stride), [0, -1i32 as u32, 0]);
+//! assert_eq!(shape.delinearize(stride), [6, 8, 42949672]);
+//!
+//! let shape = ConstShape3i32::<10, 10, 10>;
+//! let stride = shape.linearize([0, -1, 0]);
+//! assert_eq!(stride, -10);
+//!
+//! // Delinearize works with signed coordinates.
+//! assert_eq!(shape.delinearize(stride), [0, -1, 0]);
 //! ```
 
 mod const_shape;
